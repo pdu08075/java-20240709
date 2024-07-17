04a8ee720a26c199eeebda430f71e64f0264464b
pdu08075 <pdu08075>
2024-07-17 17:24:42 +0900

    repository commit

---
+++ firstProject/src/firstProject/Hello.java
@@ -0,0 +1,15 @@
+package firstProject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("두번쨰 코드!");
+//		System.out.println("세번쨰 코드!");
+		System.out.println("네번째 코드!"); // 네번쨰 코드
+		
+		/*System.out.println("다섯번째 코드!");
+		System.out.println("여섯번째 코드!");*/
+		System.out.println(/*"일곱번째 코드!"*/ "여덟번째 코드!");
+	}
+
+}
